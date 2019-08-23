b12d9092940ba4e8199f42f8c9667c81d7591595
Mark Canlas <github@htmlism.com>
2019-08-23 14:49:45 -0400

    add exists

--- core/src/main/scala/com/htmlism/Fact.scala
+++ core/src/main/scala/com/htmlism/Fact.scala
@@ -2,6 +2,10 @@ package com.htmlism
 
 sealed trait Fact
 
+case class TypeClassForStarExists(typeClass: TypeClassForStar)
+
+case class TypeClassForStarStarExists(typeClass: TypeClassForStarStar)
+
 case class TypeClassForStarHasInstance(typeClass: TypeClassForStar, plainType: PlainType) extends Fact
 
 case class TypeClassForStarStarHasInstance(typeClass: TypeClassForStarStar, kind: Kind) extends Fact
